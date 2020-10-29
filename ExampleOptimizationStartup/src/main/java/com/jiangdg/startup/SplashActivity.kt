ae7ed0ee6f690660700b0fc405a99bea3554be88
jiangdongguo <jiangdongguo@yy.com>
2020-10-29 17:40:21 +0800

    APP启动优化案例

---
+++ ExampleOptimizationStartup/src/main/java/com/jiangdg/startup/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.jiangdg.startup
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * author: jiangdg
+ * date: 2020/10/29 5:36 PM
+ * description:
+ */
+class SplashActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ ExampleOptimizationStartup/src/main/java/com/jiangdg/startup/StartupApplication.kt
@@ -0,0 +1,14 @@
+package com.jiangdg.startup
+
+import android.app.Application
+
+/**
+ * author: jiangdg
+ * date: 2020/10/29 5:35 PM
+ * description:
+ */
+class StartupApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
